21cc049ce9889545624a8779cf8b9fd3ee38a7db
Olexander Pavliuk <ipz232_poo@student.ztu.edu.ua>
2025-05-30 12:12:12 +0300

    Lab 4 Task 3: Observer pattern implementation

---
+++ lab-4/Observer/LightElementNode.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class LightElementNode : LightNode
+{
+    public string TagName { get; set; }
+    public DisplayType Display { get; set; }
+    public ClosingType Closing { get; set; }
+    public List<string> CssClasses { get; set; } = new();
+    public List<LightNode> Children { get; set; } = new();
+
+    private Dictionary<string, List<Action>> _eventListeners = new();
+
+    public LightElementNode(string tagName, DisplayType display, ClosingType closing)
+    {
+        TagName = tagName;
+        Display = display;
+        Closing = closing;
+    }
+
+    public void AddChild(LightNode node)
+    {
+        Children.Add(node);
+    }
+
+    public void AddEventListener(string eventType, Action callback)
+    {
+        if (!_eventListeners.ContainsKey(eventType))
+        {
+            _eventListeners[eventType] = new List<Action>();
+        }
+        _eventListeners[eventType].Add(callback);
+    }
+
+    public void TriggerEvent(string eventType)
+    {
+        if (_eventListeners.ContainsKey(eventType))
+        {
+            foreach (var callback in _eventListeners[eventType])
+            {
+                callback.Invoke();
+            }
+        }
+    }
+
+    private string GetClassString()
+    {
+        return CssClasses.Count > 0 ? $" class=\"{string.Join(" ", CssClasses)}\"" : "";
+    }
+
+    public override string InnerHTML
+    {
+        get
+        {
+            var sb = new StringBuilder();
+            foreach (var child in Children)
+            {
+                sb.Append(child.OuterHTML);
+            }
+            return sb.ToString();
+        }
+    }
+
+    public override string OuterHTML
+    {
+        get
+        {
+            string classAttr = GetClassString();
+            if (Closing == ClosingType.Single)
+                return $"<{TagName}{classAttr}/>";
+
+            return $"<{TagName}{classAttr}>{InnerHTML}</{TagName}>";
+        }
+    }
+}
---
+++ lab-4/Observer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var div = new LightElementNode("div", DisplayType.Block, ClosingType.Paired);
+        div.CssClasses.Add("container");
+
+        var heading = new LightElementNode("h1", DisplayType.Block, ClosingType.Paired);
+        heading.AddChild(new LightTextNode("Welcome to LightHTML!"));
+
+        var paragraph = new LightElementNode("p", DisplayType.Block, ClosingType.Paired);
+        paragraph.AddChild(new LightTextNode("This is a paragraph in our custom HTML renderer."));
+
+        var img = new LightElementNode("img", DisplayType.Inline, ClosingType.Single);
+        img.CssClasses.Add("thumbnail");
+
+        // Event listeners
+        heading.AddEventListener("click", () => Console.WriteLine("Heading clicked!"));
+        img.AddEventListener("mouseover", () => Console.WriteLine("Mouse over image!"));
+
+        div.AddChild(heading);
+        div.AddChild(paragraph);
+        div.AddChild(img);
+
+        Console.WriteLine("=== OUTER HTML ===");
+        Console.WriteLine(div.OuterHTML);
+
+        Console.WriteLine("\n=== INNER HTML ===");
+        Console.WriteLine(div.InnerHTML);
+
+        Console.WriteLine("\n=== SIMULATING EVENTS ===");
+        heading.TriggerEvent("click");
+        img.TriggerEvent("mouseover");
+
+        Console.WriteLine("\nPress Enter to exit...");
+        Console.ReadLine();
+    }
+}
